c71441aece46c010949261c555cf16c52697da86
blueBirdseyeSuit <zhasulan.abdigaliev@gmail.com>
2025-05-06 17:11:35 +0500

    Added BG to create-certificate page

--- src/app/(pages)/create-certificate/page.tsx
+++ src/app/(pages)/create-certificate/page.tsx
@@ -8,6 +8,8 @@ import { redirect } from "next/navigation";
 import { useSession } from "next-auth/react";
 import InputField from "@/components/InputField";
 import Button from "@/components/Button";
+import Image from "next/image";
+import createCertBg from "../../../../public/images/createCertBg.png"
 
 export default function CreateCertificatePage() {
     const { data: session, status } = useSession();
@@ -94,7 +96,8 @@ export default function CreateCertificatePage() {
     };
 
     return (
-        <main className="min-h-screen px-4 py-8 mt-44 flex flex-col items-center gap-24">
+        <main className="min-h-screen px-4 py-8 mt-44 flex flex-col items-center gap-24 relative">
+            <Image src={createCertBg} alt="bg" className="absolute right-[300px] top-[-240px]"/>
             {/* Main Heading */}
             <h1 className="font-bold text-green text-5xl p-5 rounded-lg bg-[#4d4d4d10] border-borderdefault backdrop-blur-xl border-[3px] w-fit top-12">
                 Create Certificate
@@ -114,86 +117,61 @@ export default function CreateCertificatePage() {
                 </Alert>
             </Snackbar>
             <div className="flex flex-col items-center p-5 rounded-lg bg-[#4d4d4d10] border-borderdefault border-[3px] backdrop-blur-xl gap-10">
-            <div className="flex gap-5">
-                <div className="flex flex-col gap-5">
+                <div className="flex gap-5">
                     <div className="flex flex-col gap-5">
-                        <h2 className="font-bold text-green text-2xl">
-                            Information about Receiver
-                        </h2>
-                        <div>
-                            <span>IIN of Receiver</span>
-                            <InputField
-                                value={recipientIIN}
-                                placeholder="Enter IIN of receiver..."
-                                onChange={(e) =>
-                                    setRecipientIIN(e.target.value)
-                                }
-                                type="text"
-                            />
-                        </div>
-                        <div className="h-[3px] min-w-full bg-borderdefault"></div>
-                        <div>
+                        <div className="flex flex-col gap-5">
                             <h2 className="font-bold text-green text-2xl">
-                                Information about Signatory
+                                Information about Receiver
                             </h2>
                             <div>
-                                <span>Certificate type</span>
-                                {/* I will change style of Select later... */}
-                                <div className="w-full flex flex-col gap-5">
-                                    <Select
-                                        fullWidth
-                                        displayEmpty
-                                        value={issuerType}
-                                        onChange={(e) =>
-                                            setIssuerType(e.target.value)
-                                        }
-                                        variant="outlined"
-                                        sx={{
-                                            color: "#d9d9d940",
-                                            ".MuiSelect-icon": {
-                                                color: "#4d4d4d10",
-                                                backgroundColor: "4d4d4d10",
-                                            },
-                                            marginBottom: "0",
-                                        }}
-                                    >
-                                        <MenuItem value="" disabled>
-                                            Select
-                                        </MenuItem>
-                                        <MenuItem value="person">
-                                            Person
-                                        </MenuItem>
-                                        <MenuItem value="organisation">
-                                            Organisation
-                                        </MenuItem>
-                                    </Select>
+                                <span>IIN of Receiver</span>
+                                <InputField
+                                    value={recipientIIN}
+                                    placeholder="Enter IIN of receiver..."
+                                    onChange={(e) =>
+                                        setRecipientIIN(e.target.value)
+                                    }
+                                    type="text"
+                                />
+                            </div>
+                            <div className="h-[3px] min-w-full bg-borderdefault"></div>
+                            <div>
+                                <h2 className="font-bold text-green text-2xl">
+                                    Information about Signatory
+                                </h2>
+                                <div>
+                                    <span>Certificate type</span>
+                                    {/* I will change style of Select later... */}
+                                    <div className="w-full flex flex-col gap-5">
+                                        <Select
+                                            fullWidth
+                                            displayEmpty
+                                            value={issuerType}
+                                            onChange={(e) =>
+                                                setIssuerType(e.target.value)
+                                            }
+                                            variant="outlined"
+                                            sx={{
+                                                color: "#d9d9d940",
+                                                ".MuiSelect-icon": {
+                                                    color: "#4d4d4d10",
+                                                    backgroundColor: "4d4d4d10",
+                                                },
+                                                marginBottom: "0",
+                                            }}
+                                        >
+                                            <MenuItem value="" disabled>
+                                                Select
+                                            </MenuItem>
+                                            <MenuItem value="person">
+                                                Person
+                                            </MenuItem>
+                                            <MenuItem value="organisation">
+                                                Organisation
+                                            </MenuItem>
+                                        </Select>
 
-                                    {issuerType === "person" && (
-                                        <div>
-                                            <span>Signatory's IIN</span>
-                                            <InputField
-                                                placeholder="Enter IIN of Signatory..."
-                                                type="text"
-                                                value={issuerIIN}
-                                                onChange={(e) =>
-                                                    setIssuerIIN(e.target.value)
-                                                }
-                                            />
-                                        </div>
-                                    )}
-                                    {issuerType === "organisation" && (
-                                        <>
-                                            <div>
-                                                <span>BIN</span>
-                                                <InputField
-                                                    placeholder="Enter BIN..."
-                                                    type="text"
-                                                    value={BIN}
-                                                    onChange={(e) =>
-                                                        setBIN(e.target.value)
-                                                    }
-                                                />
-                                            </div>
+                                        {issuerType === "person" && (
                                             <div>
                                                 <span>Signatory's IIN</span>
                                                 <InputField
@@ -207,55 +185,86 @@ export default function CreateCertificatePage() {
                                                     }
                                                 />
                                             </div>
-                                        </>
-                                    )}
+                                        )}
+                                        {issuerType === "organisation" && (
+                                            <>
+                                                <div>
+                                                    <span>BIN</span>
+                                                    <InputField
+                                                        placeholder="Enter BIN..."
+                                                        type="text"
+                                                        value={BIN}
+                                                        onChange={(e) =>
+                                                            setBIN(
+                                                                e.target.value
+                                                            )
+                                                        }
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <span>Signatory's IIN</span>
+                                                    <InputField
+                                                        placeholder="Enter IIN of Signatory..."
+                                                        type="text"
+                                                        value={issuerIIN}
+                                                        onChange={(e) =>
+                                                            setIssuerIIN(
+                                                                e.target.value
+                                                            )
+                                                        }
+                                                    />
+                                                </div>
+                                            </>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    <div className="w-[3px] min-h-full bg-borderdefault"></div>
+                    <div className="flex flex-col gap-5">
+                        <h2 className="font-bold text-green text-2xl">
+                            Certificate Details
+                        </h2>
+                        <div>
+                            <span>Certificate title</span>
+                            <InputField
+                                value={certificateTheme}
+                                placeholder="Enter Certificate title..."
+                                onChange={(e) =>
+                                    setCertificateTheme(e.target.value)
+                                }
+                                type="text"
+                            />
+                        </div>
+                        <div>
+                            <span>Certificate's body</span>
+                            <InputField
+                                value={certificateBody}
+                                placeholder="Enter Certificate's body..."
+                                onChange={(e) =>
+                                    setCertificateBody(e.target.value)
+                                }
+                                type="text"
+                            />
+                        </div>
+                        <div>
+                            <span>Date of Issue</span>
+                            <InputField
+                                value={dateOfIssue}
+                                placeholder="Enter Date of Issue..."
+                                onChange={(e) => setDateOfIssue(e.target.value)}
+                                type="date"
+                            />
+                        </div>
+                    </div>
                 </div>
-                <div className="w-[3px] min-h-full bg-borderdefault"></div>
-                <div className="flex flex-col gap-5">
-                    <h2 className="font-bold text-green text-2xl">
-                        Certificate Details
-                    </h2>
-                    <div>
-                        <span>Certificate title</span>
-                        <InputField
-                            value={certificateTheme}
-                            placeholder="Enter Certificate title..."
-                            onChange={(e) =>
-                                setCertificateTheme(e.target.value)
-                            }
-                            type="text"
-                        />
-                    </div>
-                    <div>
-                        <span>Certificate's body</span>
-                        <InputField
-                            value={certificateBody}
-                            placeholder="Enter Certificate's body..."
-                            onChange={(e) => setCertificateBody(e.target.value)}
-                            type="text"
-                        />
-                    </div>
-                    <div>
-                        <span>Date of Issue</span>
-                        <InputField
-                            value={dateOfIssue}
-                            placeholder="Enter Date of Issue..."
-                            onChange={(e) => setDateOfIssue(e.target.value)}
-                            type="date"
-                        />
-                    </div>
+                <div>
+                    <Button
+                        onClick={handleIssueCertificate}
+                        title="Issue Certificate"
+                    />
                 </div>
-            </div>
-            <div>
-                <Button
-                    onClick={handleIssueCertificate}
-                    title="Issue Certificate"
-                />
-            </div>
             </div>
         </main>
     );
