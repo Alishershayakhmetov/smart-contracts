1a8519ca8e26531172e99a2bc45e97375f6b3a66
blueBirdseyeSuit <zhasulan.abdigaliev@gmail.com>
2025-05-06 23:28:00 +0500

    Changed user certificates design and code to normal. Added NoData icon

--- src/app/(pages)/certificates/page.tsx
+++ src/app/(pages)/certificates/page.tsx
@@ -8,78 +8,84 @@ import axios from "axios";
 import CertificatesLoading from "@/components/certificateLoading";
 import { Certificate } from "@/interfaces/interface";
 import Header from "@/components/Header";
+import NoDataIcon from "@/components/icons/NoDataIcon";
 
 export default function AllUserCertificatesPage() {
-  const { data: session, status } = useSession();
-  const [certificates, setCertificates] = useState<Certificate[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+    const { data: session, status } = useSession();
+    const [certificates, setCertificates] = useState<Certificate[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    if (status === "unauthenticated") {
-      redirect("/login");
-    }
+    useEffect(() => {
+        if (status === "unauthenticated") {
+            redirect("/login");
+        }
 
-    const fetchCertificates = async () => {
-      try {
-        setIsLoading(true);
-        const response = await axios.get("/api/certificate", {
-          withCredentials: true, // This ensures cookies are sent with the request
-        });
-        setCertificates(response.data);
-      } catch (error) {
-        console.error("Error fetching certificates:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+        const fetchCertificates = async () => {
+            try {
+                setIsLoading(true);
+                const response = await axios.get("/api/certificate", {
+                    withCredentials: true, // This ensures cookies are sent with the request
+                });
+                setCertificates(response.data);
+            } catch (error) {
+                console.error("Error fetching certificates:", error);
+            } finally {
+                setIsLoading(false);
+            }
+        };
 
-    if (status === "authenticated") {
-      fetchCertificates();
-    }
-  }, [status]);
+        if (status === "authenticated") {
+            fetchCertificates();
+        }
+    }, [status]);
 
-  return (
-    <main className="min-h-screen bg-[#0B0F0C] text-white px-4 py-8">
-      {/* Header */}
-      <Header />
+    return (
+        <main className="min-h-screen px-4 py-8">
+            {/* Main Heading */}
+            <h1 className="text-center text-5xl font-bold text-green mb-12 border border-borderdefault p-5 w-fit mx-auto rounded-lg mt-[150]">
+                All Certificates
+            </h1>
 
-      {/* Main Heading */}
-      <h1 className="text-center text-3xl font-bold text-lime-400 mb-12 border border-lime-400 py-4 max-w-md mx-auto rounded-md mt-[150]">
-        All Certificates
-      </h1>
+            {isLoading && CertificatesLoading()}
 
-      {isLoading && CertificatesLoading()}
-
-      <article className="max-w-5xl mx-auto">
-        {isLoading ? (
-          <div className="text-center">Loading certificates...</div>
-        ) : certificates.length === 0 ? (
-          <div className="text-center">No certificates found</div>
-        ) : (
-          <div className="grid gap-4">
-            {certificates.map((certificate) => (
-              <Link
-                href={`/certificates/${certificate.id}`}
-                key={certificate.id}
-              >
-                <div className="border border-gray-700 p-4 rounded-lg hover:border-lime-400 transition-colors">
-                  <h3 className="text-xl font-semibold text-lime-400">
-                    {certificate.certificateTheme}
-                  </h3>
-                  <p className="text-gray-300">
-                    Recipient IIN: {certificate.recipientIIN}
-                  </p>
-                  <p className="text-gray-400 text-sm">
-                    Issued on:{" "}
-                    {new Date(certificate.dateOfIssue).toLocaleDateString()}
-                  </p>
-                  {/* Add more certificate details as needed */}
-                </div>
-              </Link>
-            ))}
-          </div>
-        )}
-      </article>
-    </main>
-  );
+            <article className="max-w-5xl mx-auto flex flex-col items-center">
+                {isLoading ? (
+                    <div className="text-center">Loading certificates...</div>
+                ) : certificates.length === 0 ? (
+                    <div className="flex flex-col gap-5 items-center">
+                        <NoDataIcon />
+                        <h2 className="font-bold text-green text-center text-2xl">
+                            No certificates found.
+                        </h2>
+                    </div>
+                ) : (
+                    <div className="grid gap-4">
+                        {certificates.map((certificate) => (
+                            <Link
+                                href={`/certificates/${certificate.id}`}
+                                key={certificate.id}
+                            >
+                                <div className="border border-gray-700 p-4 rounded-lg hover:border-lime-400 transition-colors">
+                                    <h3 className="text-xl font-semibold text-lime-400">
+                                        {certificate.certificateTheme}
+                                    </h3>
+                                    <p className="text-gray-300">
+                                        Recipient IIN:{" "}
+                                        {certificate.recipientIIN}
+                                    </p>
+                                    <p className="text-gray-400 text-sm">
+                                        Issued on:{" "}
+                                        {new Date(
+                                            certificate.dateOfIssue
+                                        ).toLocaleDateString()}
+                                    </p>
+                                    {/* Add more certificate details as needed */}
+                                </div>
+                            </Link>
+                        ))}
+                    </div>
+                )}
+            </article>
+        </main>
+    );
 }
---
+++ src/components/icons/NoDataIcon.tsx
@@ -0,0 +1,84 @@
+import * as React from "react";
+
+const SvgIcon: React.FC<React.SVGProps<SVGElement>> = (props) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="201"
+        height="200"
+        fill="none"
+        viewBox="0 0 84 83"
+    >
+        <path
+            stroke="url(#paint0_linear_188_642)"
+            strokeOpacity="0.5"
+            d="M56.042 57.58a20.63 20.63 0 0 0-14.584-6.04c-5.47 0-10.01 1.465-13.877 5.333"
+        ></path>
+        <circle
+            cx="30.075"
+            cy="45.112"
+            r="3.612"
+            fill="url(#paint1_linear_188_642)"
+            fillOpacity="0.5"
+        ></circle>
+        <circle
+            cx="53.545"
+            cy="45.112"
+            r="3.612"
+            fill="url(#paint2_linear_188_642)"
+            fillOpacity="0.5"
+        ></circle>
+        <path
+            stroke="url(#paint3_linear_188_642)"
+            strokeOpacity="0.5"
+            d="M14.576.5h31.922v21.547a4.4 4.4 0 0 0 4.39 4.39h21.547V79.11c0 1.88-1.51 3.391-3.39 3.391h-54.47c-1.879 0-3.39-1.51-3.39-3.39V3.89c0-1.88 1.511-3.39 3.39-3.39Zm38.599 0c.906 0 1.767.353 2.4.987v.001l15.872 15.887c.635.635.988 1.497.988 2.386v.489h-19.75V.5z"
+        ></path>
+        <defs>
+            <linearGradient
+                id="paint0_linear_188_642"
+                x1="41.369"
+                x2="41.016"
+                y1="42.994"
+                y2="57.207"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#fff" stopOpacity="0.1"></stop>
+                <stop offset="1" stopColor="#8CD813"></stop>
+            </linearGradient>
+            <linearGradient
+                id="paint1_linear_188_642"
+                x1="26.441"
+                x2="30.053"
+                y1="41.523"
+                y2="45.135"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#fff" stopOpacity="0.1"></stop>
+                <stop offset="1" stopColor="#8CD813"></stop>
+            </linearGradient>
+            <linearGradient
+                id="paint2_linear_188_642"
+                x1="49.91"
+                x2="53.523"
+                y1="41.523"
+                y2="45.135"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#fff" stopOpacity="0.1"></stop>
+                <stop offset="1" stopColor="#8CD813"></stop>
+            </linearGradient>
+            <linearGradient
+                id="paint3_linear_188_642"
+                x1="10.491"
+                x2="50.331"
+                y1="0.259"
+                y2="30.139"
+                gradientUnits="userSpaceOnUse"
+            >
+                <stop stopColor="#fff" stopOpacity="0.1"></stop>
+                <stop offset="1" stopColor="#8CD813"></stop>
+            </linearGradient>
+        </defs>
+    </svg>
+);
+
+export default SvgIcon;
